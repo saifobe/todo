3d7050fda580b2e0ddfd3ca128689d79d307e1eb
saifobe <obeidatsaif93@gmail.com>
2023-02-06 09:59:48 +0000

    a JS code has been Added

---
+++ app.js
@@ -0,0 +1,50 @@
+"use strict";
+var name;
+var name = prompt("Enter Your Name ");
+
+var gender = prompt("What is your Gender ?");
+
+if(gender == "male" || gender == "female")
+{
+    alert("you entered " + " " + gender);
+}
+else{
+    alert("The Answer Should Be male or female please")
+}
+
+var age  = prompt("Enter Your Age please ");
+
+if(age <= 0)
+{
+    alert("INCORRECT AGE !!!");
+}
+else{
+    alert("your age is" + " " + age);
+}
+
+var massage = confirm("Do you want to Read the Welcoming Massage ?");
+
+if(massage = true)
+{
+    if(gender == "male"  && age >= 0 )
+    {
+        alert("Welcome Mr." + name);
+    }
+
+    else if(gender == "female" && age >= 0 )
+    {
+        alert("Welcome Mrs." + name);
+    }
+
+    else
+    {
+        alert("welcome");
+    }
+    
+}
+
+else
+{
+   alert("You skipped the welcoming masseage");
+}
+
